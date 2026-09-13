c7df79ed0be1533f06b9529c0154c0aa4cf797d7
Gagan-GowdaMG <gagangowdamg84@gmail.com>
2026-09-14 00:57:13 +0530

    Time: 54 ms (10.43%) | Memory: 19.2 MB (70.02%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -7,11 +7,11 @@ class Solution:
             if -2**31<c>(2**31)-1 :
                 return 0
             else:
-                return int(c)*-1
+                return c*-1
         else:
             c=a[::-1]
             c=int(c)
             if -2**31<c>(2**31)-1 :
                 return 0
             else:
-                return int(c)
+                return c
